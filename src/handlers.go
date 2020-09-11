5bbbc9934ab95e1f10c0dd3fc2ae5154680048f1
hsgilliland <jo-ash@procentrica.co.za>
2020-09-11 15:29:51 +0200

    did baseline calls

--- src/handlers.go
+++ src/handlers.go
@@ -1,1 +1,167 @@
 package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func (s *Server) handlegetasset() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Handle Get Asset Has Been Called...")
+		// retrieving the ID of the asset that is requested.
+		getAsset := AssetID{}
+		// convert received JSON payload into the declared struct.
+		err := json.NewDecoder(r.Body).Decode(&getAsset)
+		//check for errors when converting JSON payload into struct.
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "Bad JSON provided to export asset")
+			return
+		}
+
+		//create byte array from JSON payload
+		requestByte, _ := json.Marshal(getAsset)
+
+		//post to crud service
+		req, respErr := http.Post("http://"+config.CRUDHost+":"+config.CRUDPort+"/asset", "application/json", bytes.NewBuffer(requestByte))
+
+		//check for response error of 500
+		if respErr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, respErr.Error())
+			fmt.Println("Error in communication with CRUD service endpoint for export asset")
+			return
+		}
+		if req.StatusCode != 200 {
+			fmt.Fprint(w, "Request to DB can't be completed...")
+			fmt.Println("Unable to asset export")
+		}
+		if req.StatusCode == 500 {
+			w.WriteHeader(500)
+
+			bodyBytes, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bodyString := string(bodyBytes)
+			fmt.Fprintf(w, "Request to DB can't be completed..."+bodyString)
+			fmt.Println("Request to DB can't be completed..." + bodyString)
+			return
+		}
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err.Error())
+			fmt.Println("Exportation is not able to be completed by internal error")
+			return
+		}
+
+		//close the request
+		defer req.Body.Close()
+
+		//create new response struct
+		var assetResponse AssetRegisterResponse
+
+		//decode request into decoder which converts to the struct
+		decoder := json.NewDecoder(req.Body)
+
+		err = decoder.Decode(&assetResponse)
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err.Error())
+			fmt.Println("Error occured in decoding asset response")
+			return
+		}
+		js, jserr := json.Marshal(assetResponse)
+		if jserr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, jserr.Error())
+			fmt.Println("Error occured when trying to marshal the response to export asset")
+			return
+		}
+
+		//return back to Front-End user
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(js)
+	}
+}
+
+func (s *Server) handlegetassets() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Handle Get Assets Has Been Called...")
+		getAsset := AssetID{}
+		err := json.NewDecoder(r.Body).Decode(&getAsset)
+		//handle for bad JSON provided
+
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err.Error())
+			fmt.Println("Could not read body of request into proper JSON format for getting assets.\n Please check that your data is in the correct format.")
+			return
+		}
+
+		//create byte array from JSON payload
+		requestByte, _ := json.Marshal(getAsset)
+
+		//post to crud service
+		req, respErr := http.Post("http://"+config.CRUDHost+":"+config.CRUDPort+"/assets", "application/json", bytes.NewBuffer(requestByte))
+
+		//check for response error of 500
+		if respErr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, respErr.Error())
+			fmt.Println("Error in communication with CRUD service endpoint for request to retrieve advertisement information")
+			return
+		}
+		if req.StatusCode != 200 {
+			w.WriteHeader(req.StatusCode)
+			fmt.Fprint(w, "Request to DB can't be completed...")
+			fmt.Println("Request to DB can't be completed...")
+		}
+		if req.StatusCode == 500 {
+			w.WriteHeader(500)
+			bodyBytes, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bodyString := string(bodyBytes)
+			fmt.Fprintf(w, "An internal error has occured whilst trying to get asset data"+bodyString)
+			fmt.Println("An internal error has occured whilst trying to get asset data" + bodyString)
+			return
+		}
+
+		//close the request
+		defer req.Body.Close()
+
+		//create new response struct for JSON list
+		assetsList := AssetList{}
+		assetsList.Assets = []AssetRegisterResponse{}
+
+		//decode request into decoder which converts to the struct
+		decoder := json.NewDecoder(req.Body)
+		err1 := decoder.Decode(&assetsList)
+		if err1 != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err1.Error())
+			fmt.Println("Error occured in decoding get Messages response ")
+			return
+		}
+		//convert struct back to JSON
+		js, jserr := json.Marshal(assetsList)
+		if jserr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, jserr.Error())
+			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
+			return
+		}
+
+		//return success back to Front-End user
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(js)
+	}
+}
