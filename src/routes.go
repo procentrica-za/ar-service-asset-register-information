706bb52b871c6a96cc592d669bf9a442ceff9360
hsgilliland <jo-ash@procentrica.co.za>
2020-11-11 09:28:49 +0200

    functionality to pull assets by location complete

--- src/routes.go
+++ src/routes.go
@@ -4,4 +4,7 @@ package main
 func (s *Server) routes() {
 	s.router.HandleFunc("/asset", s.handlegetasset()).Methods("GET")
 	s.router.HandleFunc("/assets", s.handlegetassets()).Methods("GET")
+
+	//Demo Routes
+	s.router.HandleFunc("/funclocdetails", s.handlegetfunclocCurrentDetails()).Methods("GET")
 }
